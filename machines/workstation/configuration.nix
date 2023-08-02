40371d503123faf02c790ba5a484c5f1948c7ecb
Joel Pepper <pepper@bronze-deer.de>
2023-08-02 19:43:37 +0200

    feat(sound): Enable pipewire

--- machines/workstation/configuration.nix
+++ machines/workstation/configuration.nix
@@ -10,6 +10,8 @@
       ./hardware-configuration.nix
       # Monitor setup
       ./autorandr.nix
+      # Enable Pipewire for sound
+      ./pipewire
     ];
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
---
+++ machines/workstation/pipewire/default.nix
@@ -0,0 +1,15 @@
+{ config, pkgs, ... }:
+{
+  # Do not use default sound enable, conflicts with pipewire
+  sound.enable = false;
+
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+    wireplumber.enable = true;
+  };
+}
