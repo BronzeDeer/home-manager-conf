39e11810f40242d0f735e67ee307285ae2951042
Joel Pepper <pepper@bronze-deer.de>
2023-08-13 22:15:46 +0200

    feat(nvidia): Enable support for better suspend
    Compare https://download.nvidia.com/XFree86/Linux-x86_64/435.17/README/powermanagement.html
    Support is currently considered experimental

--- modules/system/nvidia/default.nix
+++ modules/system/nvidia/default.nix
@@ -10,6 +10,8 @@
   hardware.nvidia = {
     modesetting.enable = true;
     nvidiaSettings = true;
+    # Experimental support for better resume from sleep
+    powerManagement.enable = true;
   };
   services.xserver.videoDrivers = ["nvidia"];
 }
