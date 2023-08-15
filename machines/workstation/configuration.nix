81fc01d58a0619eb6240fb3bc6ddd2f4d4e21e1b
Joel Pepper <pepper@bronze-deer.de>
2023-08-16 00:30:43 +0200

    feat: add file-picker portal for gtk apps
    Allows gtk apps to find the native themed file picker dialog. Note that
    many gtk apps will already fall back to using a built in version of the
    dialog, but this will broaden support

--- machines/workstation/configuration.nix
+++ machines/workstation/configuration.nix
@@ -125,6 +125,16 @@
     home-manager
   ];
 
+  # Enable file picker for apps with portal support
+  xdg.portal = {
+    enable = true;
+    xdgOpenUsePortal = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal
+      xdg-desktop-portal-gtk
+    ];
+  };
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
