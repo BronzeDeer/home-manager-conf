7a2456bec8d20a5a51d8336f0563e4a1b68aa66a
Joel Pepper <pepper@bronze-deer.de>
2024-01-20 12:49:38 +0100

    feat(basic-tools): add more tools
    file
    ncdu
    (un)zip
    dig
    traceroute

--- modules/user/basic-tools/default.nix
+++ modules/user/basic-tools/default.nix
@@ -3,5 +3,14 @@
   home.packages = with pkgs; [
     evince # Gnome document viewer
     loupe # image viewer
+
+    dig # dns tools
+    traceroute
+
+    zip
+    unzip
+
+    file # file identifier
+    ncdu # ncurses disk usage explorer
   ];
 }
