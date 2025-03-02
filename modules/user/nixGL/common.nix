621f539a4350c7f2203442d5459b3fd2acb340a8
Joel Pepper <pepper@bronze-deer.de>
2025-03-02 16:16:11 +0100

    feat(nixGL): Do not wrap on nixOS hosts

--- modules/user/nixGL/common.nix
+++ modules/user/nixGL/common.nix
@@ -1,10 +1,12 @@
 # Adds a helper method to wrap OpenGL applications transparently on non-nixOS systems
 # The core problem is well summarized in https://alternativebit.fr/posts/nixos/nix-opengl-and-ubuntu-integration-nightmare/
 # However we use nixGL which solves the problem at build time (by including a lot of opengl libs) rather than requiring systems to have nixglhost setup
-{ config, pkgs, nixgl, ... }:
+{ config, pkgs, nixgl, osConfig ? null, ... }:
 {
 
   nixGL = {
-    packages = nixgl.packages; # you must set this or everything will be a noop
+    # If we are evaluated on a nixOS system (osConfig != null), noop,
+    # since nixOS doesn't need the nixGL wrapper
+    packages = if osConfig != null then nixgl.packages else null;
   };
 }
