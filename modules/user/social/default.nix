6fa9c1e73a0fbf3e65993dfc75e8fb64a200e766
Joel Pepper <pepper@bronze-deer.de>
2025-07-25 17:50:37 +0200

    feat(social): create .desktop entries for chromium based messengers
    having .desktop files which wrap the long chromium commands makes
    relaunching them easier without relying on the autostart script. They
    also allow integrating with the recent transition to direct launching
    from desktop file over launching as background job

--- modules/user/social/default.nix
+++ modules/user/social/default.nix
@@ -2,23 +2,41 @@
 let
   signal = pkgs.signal-desktop;
   telegram = pkgs.telegram-desktop;
+  # For the browser-only messengers, create separate chromium profiles and launch them in app mode
+  # TODO: make actual chromium based browser (Chrome, ungoogled, Brave etc) configurable
+  # For now, assume that chromium is installed and available as "chromium"
+  # TODO: icons as fixed output derivations
+  whatsapp = pkgs.makeDesktopItem {
+    name = "WhatsApp";
+    desktopName = "WhatsApp";
+    comment = "Running WhatsApp in chromium app mode";
+    exec = ''chromium --app="http://web.whatsapp.com" --user-data-dir=${config.home.homeDirectory}/.messenger-browser-profiles/whatsapp'';
+    terminal = false;
+  };
+  fb-messenger = pkgs.makeDesktopItem {
+    name = "fb-messenger";
+    desktopName = "Facebook Messenger";
+    comment = "Running Facebook Messenger in chromium app mode";
+    exec = ''chromium --app="http://www.facebook.com/messages/t/" --user-data-dir=${config.home.homeDirectory}/.messenger-browser-profiles/facebook'';
+    terminal = false;
+  };
 in
 {
   home.packages = with pkgs; [
     signal
     telegram
     discord
+    whatsapp
+    fb-messenger
     dex # platform agnostic .desktop launcher
   ];
 
-  # For the browser-only messengers, create separate chromium profiles and launch them in app mode
-  # Todo: make actual chromium based browser (Chrome, ungoogled, Brave etc) configurable
-  # For now, assume that chromium is installed and available as "chromium"
-
   userautostart.scriptInline = ''
-    chromium  --app="http://web.whatsapp.com" --user-data-dir=$HOME/.messenger-browser-profiles/whatsapp &
+    #dex ${fb-messenger}/share/applications/*.desktop
+    dex ${whatsapp}/share/applications/*.desktop
     dex ${signal}/share/applications/*.desktop
     dex ${telegram}/share/applications/*.desktop
   '';
-    #chromium  --app="http://www.facebook.com/messages/t/" --user-data-dir=$HOME/.messenger-browser-profiles/facebook &
+    #chromium  --app="http://web.whatsapp.com" --user-data-dir=$HOME/.messenger-browser-profiles/whatsapp &
+    #chromium  --app="http://www.facebook.com/messages/t/" --user-data-dir=${config.home.homeDirectory}/.messenger-browser-profiles/facebook &
 }
