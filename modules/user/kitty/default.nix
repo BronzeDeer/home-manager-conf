855e632b246ab6fb0e3d62f4aff6a3081161e638
Joel Pepper <pepper@bronze-deer.de>
2025-09-08 11:32:43 +0200

    feat(kitty): improve symbol support with explicit mapping/fallback
    kitty has a explicit font-fallback and symbol mapping features, which
    allows it to no be reliant on patched fonts (which can sometimes lead to
    weird issues).
    
    This commit configures kitty to use the un-patched
    version of the currently configured font (fira-code) and install the
    NerdFonts symbols only font, explicitly mapping it's symbols in kitty's
    settings. Other applications implementing font-fallback should now also
    have an easier time accessing nerd-font symbols even if the configured
    font is not a patched font

--- modules/user/kitty/default.nix
+++ modules/user/kitty/default.nix
@@ -13,6 +13,7 @@
   home.packages = with pkgs; [
     # Install a nerdfont patches version of our desired Font (FiraCode)
     nerd-fonts.fira-code
+    nerd-fonts.symbols-only
   ];
 
   programs.kitty = {
@@ -20,7 +21,8 @@
 
     package = (config.lib.nixGL.wrap pkgs.kitty);
 
-    font.name = "FiraCode Nerd Font Mono";
+    font.name = "Fira Mono";
+    font.package = pkgs.fira-mono;
     font.size = 10;
     shellIntegration.enableZshIntegration = true;
     #theme = "${theming.kitty-theme}";
@@ -48,5 +50,32 @@
       color14 = "${theming.bright-cyan}";
       color15 = "${theming.bright-white}";
     };
+    extraConfig = ''
+      # - Use additional nerd symbols
+      # See https://github.com/be5invis/Iosevka/issues/248
+      # See https://github.com/ryanoasis/nerd-fonts/wiki/Glyph-Sets-and-Code-Points
+      # Seti-UI + Custom
+      symbol_map U+E5FA-U+E62B Symbols Nerd Font
+      # Devicons
+      symbol_map U+E700-U+E7C5 Symbols Nerd Font
+      # Font Awesome
+      symbol_map U+F000-U+F2E0 Symbols Nerd Font
+      # Font Awesome Extension
+      symbol_map U+E200-U+E2A9 Symbols Nerd Font
+      # Material Design Icons
+      symbol_map U+F500-U+FD46 Symbols Nerd Font
+      # Weather
+      symbol_map U+E300-U+E3EB Symbols Nerd Font
+      # Octicons
+      symbol_map U+F400-U+F4A8,U+2665,U+26A1,U+F27C Symbols Nerd Font
+      # Powerline Extra Symbols
+      symbol_map U+E0A3,U+E0B4-U+E0C8,U+E0CC-U+E0D2,U+E0D4 Symbols Nerd Font
+      # IEC Power Symbols
+      symbol_map U+23FB-U+23FE,U+2b58 Symbols Nerd Font
+      # Font Logos
+      symbol_map U+F300-U+F313 Symbols Nerd Font
+      # Pomicons
+      symbol_map U+E000-U+E00D Symbols Nerd Font
+    '';
   };
 }
