a804c8d533de75f5bb58c95803088024c00b8cb9
Joel Pepper <pepper@bronze-deer.de>
2025-12-17 14:22:54 +0100

    fix(lockscreen): only launch xautolock in X11 sessions
    Since Xorg was historically the only type of graphical-session most Xorg-
    dependent services target _any_ graphical-session, including wayland sessions.
    
    This leads to a problem when xautolock and xss-lock run, but cannot
    receive non-idle events from the non-existant X-Server and always sends
    session lock events via the d-bus, which then causes hypridle to
    (correctly) launch the lock screen since the user session got locked,
    even if hypridle was correctly inhibited by user actions or an
    application.
    
    This commit changes the definition of the systemd user services for
    xautolock and xss-lock to target and require "hm-graphical-session.target"
    which is used by home-manager when launching its configured XSession via
    the .xprofile script.

--- modules/user/betterlockscreen/default.nix
+++ modules/user/betterlockscreen/default.nix
@@ -3,4 +3,25 @@
     enable = true;
     inactiveInterval = 30;
   };
+
+  # Since Xorg was historically the only type of graphical-session most Xorg dependent services
+  #   target _any_ graphical-session, including wayland sessions.
+  # Wayland services have a separate target to avoid launching under X but not vice versa
+  # Since in our setup the X11 DE is launched by hm we can use the hm-graphical-session
+  #   to target to only target non-wayland sessions
+  systemd.user.services.xautolock-session = {
+    Unit = {
+      After = [ "hm-graphical-session.target" ];
+      PartOf = [ "hm-graphical-session.target" ];
+      Requisite = [ "hm-graphical-session.target" ];
+    };
+  };
+
+  systemd.user.services.xss-lock = {
+    Unit = {
+      After = [ "hm-graphical-session.target" ];
+      PartOf = [ "hm-graphical-session.target" ];
+      Requisite = [ "hm-graphical-session.target" ];
+    };
+  };
 }
