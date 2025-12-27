b57d90b3ac3625239c9c65ee9b6c26519f9f2ce8
Joel Pepper <pepper@bronze-deer.de>
2025-12-27 14:26:02 +0100

    fix(picom): disable service on hyprland

--- modules/user/picom/default.nix
+++ modules/user/picom/default.nix
@@ -56,4 +56,17 @@ in
     };
 
   };
+
+  # Since Xorg was historically the only type of graphical-session most Xorg dependent services
+  #   target _any_ graphical-session, including wayland sessions.
+  # Wayland services have a separate target to avoid launching under X but not vice versa
+  # Since in our setup the X11 DE is launched by hm we can use the hm-graphical-session
+  #   to target to only target non-wayland sessions
+  systemd.user.services.picom = {
+    Unit = {
+      After = [ "hm-graphical-session.target" ];
+      PartOf = [ "hm-graphical-session.target" ];
+      Requisite = [ "hm-graphical-session.target" ];
+    };
+  };
 }
