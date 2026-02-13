18aee43a24741104079849b4c6704a1738ed30c5
Joel Pepper <pepper@bronze-deer.de>
2026-02-13 11:08:45 +0100

    feat(pay-respects): expand module with option to control cnf hook
    Currently, the pay-respects module unconditionally enables both
    pay-respects and nix-index and the shell integrations of both. This has
    the disadvantage of being order dependent in the final shell init file
    in determining whether the nix-index cnf handler or the pay-respects cnf
    handler takes precedence.
    
    This commit expands the pay-respects module to provide options, beside
    the general "enable" option, it also includes an "enableCommandNotFound"
    option, which will control whether the pay-respects hook tried to
    register its own handler and will also conditionally supress the options
    enabling the nix-index cnf handler to ensure consistency

--- modules/user/pay-respects/default.nix
+++ modules/user/pay-respects/default.nix
@@ -4,26 +4,53 @@
   config,
   ...
 }@inputs:
+  let
+    cfg = config.bd.pay-respects;
+  in
 {
+  options.bd.pay-respects = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+    };
 
+    enableCommandNotFound = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = ''
+        Register as command not found handler in shells with enabled integration (see programs.pay-respects.enable*shIntegration).
+        If true, will ensure that nix-index is installed as backing provider for package searches and disable its command-not-found integration
+      '';
+    };
+  };
+  
   # If our containing flake provides the precompiled nix-index-database module, use that
   imports = lib.optionals (inputs ? "nix-index-database") [
     inputs.nix-index-database.homeModules.nix-index
   ];
 
-  programs.pay-respects = {
-    enable = true;
-    enableZshIntegration = config.programs.zsh.enable;
-    enableFishIntegration = config.programs.fish.enable;
-    enableBashIntegration = config.programs.bash.enable;
-    enableNushellIntegration = config.programs.nushell.enable;
-  };
+  config = lib.mkIf cfg.enable {
 
-  # nix-index is used to allow rapid searching through available nix packages for pay-respects and other command-not-found wrappers to suggest which packages provide a certain command
-  programs.nix-index = {
-    enable = true;
-    enableZshIntegration = config.programs.zsh.enable;
-    enableFishIntegration = config.programs.fish.enable;
-    enableBashIntegration = config.programs.bash.enable;
+    programs.pay-respects = {
+      enable = true;
+      enableZshIntegration = config.programs.zsh.enable;
+      enableFishIntegration = config.programs.fish.enable;
+      enableBashIntegration = config.programs.bash.enable;
+      enableNushellIntegration = config.programs.nushell.enable;
+
+      # pay-respects registers the cnf handler by default, if we do not want that we have to pass --nocnf
+      options = lib.optionals (! cfg.enableCommandNotFound) [
+        "--nocnf"
+      ];
+    };
+
+    # nix-index is used to allow rapid searching through available nix packages for pay-respects and other command-not-found wrappers to suggest which packages provide a certain command
+    programs.nix-index = lib.optionalAttrs cfg.enableCommandNotFound {
+      enable = true;
+      # Deactivate the nix-index command-not-found hook if we are using the pay-respects hook in that shell
+      enableZshIntegration = !config.programs.pay-respects.enableZshIntegration;
+      enableFishIntegration = !config.programs.pay-respects.enableFishIntegration;
+      enableBashIntegration = !config.programs.pay-respects.enableBashIntegration;
+    };
   };
 }
